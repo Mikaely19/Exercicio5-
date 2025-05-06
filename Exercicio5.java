e33e80f114d3e9cf899ecccfc211713e82b8c9ea
Mikaely19 <125111409306@ulife.com.br>
2025-05-05 23:33:46 -0300

    Update Exercicio5.java

--- Exercicio5.java
+++ Exercicio5.java
@@ -4,27 +4,22 @@ public class App {
         int[] numeros = new int[12];
         int soma = 0;
 
-        // Recebe os 12 números do usuário
-        System.out.println("Digite 12 números:");
+        System.out.println("Digite osnúmeros:");
         for (int i = 0; i < 12; i++) {
             System.out.print("Número " + (i + 1) + ": ");
             numeros[i] = scanner.nextInt();
             soma += numeros[i];
         }
 
-        // Calcula a média
-        double media = (double) soma / 12;
+        double media = (double) soma / int;
         System.out.println("A média dos valores é: " + media);
-
-        // Conta quantos elementos são maiores que a média
+        
         int countMaiorQueMedia = 0;
         for (int num : numeros) {
             if (num > media) {
                 countMaiorQueMedia++;
             }
         }
-
-        // Exibe o resultado
         System.out.println("Quantidade de elementos maiores que a média: " + countMaiorQueMedia);
         
         scanner.close();
